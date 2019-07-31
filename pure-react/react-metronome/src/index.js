f99a4a1a10014622b9ceb0cb46c5d76aefe561ee
DickensJuma <dickensjuma13@gmail.com>
2019-07-31 20:37:00 +0300

    added react-metronome app

---
+++ pure-react/react-metronome/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Metronome from './Metronome';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Metronome/>, document.getElementById('root'));
+registerServiceWorker();
+
